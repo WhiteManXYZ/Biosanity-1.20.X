6127d0b7e99208ffa1654407b06712e9505f0a67
WhiteManXYZ <vladimir.gavrilyul10@gmail.com>
2026-03-07 23:17:07 +0100

    - Majority params added Parameters (such as destroy time etc.) now inherit original block with calculating multiplier coeff that based on rot stage.

--- src/main/java/net/whiteman/biosanity/block/custom/neoplasm/NeoplasmRotBlock.java
+++ src/main/java/net/whiteman/biosanity/block/custom/neoplasm/NeoplasmRotBlock.java
@@ -4,12 +4,17 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -31,6 +36,10 @@ public class NeoplasmRotBlock extends NeoplasmBlock implements EntityBlock {
     private static final int MIN_INFECTION_SPEED = 150;
     private static final int MAX_INFECTION_SPEED = 240;
     private static final double NEOPLASM_ROT_DROP_CHANCE = 0.2;
+    private static final float[] DIG_SPEED_MULTIPLIERS = {1.0f, 1.5f, 2.0f};
+    private static final float[] EXPLOSION_RESISTANCE_MULTIPLIERS = {1.0f, 0.7f, 0.4f};
+    private static final float[] FLAME_MULTIPLIERS = {1.0f, 0.8f, 0.5f};
+    private static final float[] FALL_DAMAGE_MULTIPLIERS = {1.0f, 0.85f, 0.7f};
 
     public NeoplasmRotBlock(Properties properties) {
         super(properties);
@@ -53,8 +62,8 @@ public class NeoplasmRotBlock extends NeoplasmBlock implements EntityBlock {
 
     @Override
     public void tick(@NotNull BlockState state, @NotNull ServerLevel level, @NotNull BlockPos pos, @NotNull RandomSource random) {
-        // Infect 1 block in random direction only if
-        // this block is in devour map
+        // Infect 1 block in random direction
+        // only if this block is in devour map
         Direction[] directions = Direction.values();
         Direction randomDir = directions[random.nextInt(directions.length)];
 
@@ -85,18 +94,17 @@ public class NeoplasmRotBlock extends NeoplasmBlock implements EntityBlock {
     }
 
     @Override
-    public void randomTick(@NotNull BlockState pState, @NotNull ServerLevel pLevel, @NotNull BlockPos pPos, @NotNull RandomSource pRandom) {
+    public void randomTick(@NotNull BlockState state, @NotNull ServerLevel level, @NotNull BlockPos pos, @NotNull RandomSource random) {
         // Rotting over time
-        if (pLevel.getBlockEntity(pPos) instanceof NeoplasmRotBlockEntity be) {
+        if (level.getBlockEntity(pos) instanceof NeoplasmRotBlockEntity be) {
             int currentStage = be.getOverlayStage();
             if (currentStage < NeoplasmRotBlockEntity.MAX_STAGES - 1) {
                 be.setInfectionStage(currentStage + 1);
             }
         }
-        // If block schedule chain is broken, trying to launch it again
-        if (!pLevel.getBlockTicks().hasScheduledTick(pPos, this)) {
-            pLevel.scheduleTick(pPos, this, 1);
-        }
+        // Random tick for faster infect and
+        // if block schedule chain is broken, trying to launch it again
+        tick(state, level, pos, random);
     }
 
     @Override
@@ -133,4 +141,92 @@ public class NeoplasmRotBlock extends NeoplasmBlock implements EntityBlock {
         }
         super.playerDestroy(level, player, pos, state, pBlockEntity, pTool);
     }
+
+    @Override
+    public float getDestroyProgress(@NotNull BlockState state, @NotNull Player player, BlockGetter level, @NotNull BlockPos pos) {
+        if (level.getBlockEntity(pos) instanceof NeoplasmRotBlockEntity be) {
+            // Better copy destroy speed parameters from original
+            BlockState original = be.getOriginalState();
+            float originalProgress = original.getDestroyProgress(player, level, pos);
+
+            // To prevent breaking our block instantly
+            if (originalProgress <= 0 || original.isAir()) {
+                return super.getDestroyProgress(state, player, level, pos);
+            }
+
+            return originalProgress * be.getMultiplier(DIG_SPEED_MULTIPLIERS);
+        }
+        return super.getDestroyProgress(state, player, level, pos);
+    }
+
+    @Override
+    public boolean isFlammable(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return true;
+    }
+
+    @Override
+    public int getFlammability(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        if (level.getBlockEntity(pos) instanceof NeoplasmRotBlockEntity be) {
+            // Better copy flammability from original
+            BlockState original = be.getOriginalState();
+            int originalFlammability = original.getFlammability(level, pos, direction);
+
+            int resultFlammability = (int) Math.ceil(originalFlammability * be.getMultiplier(FLAME_MULTIPLIERS));
+
+            // Never allow to be nonflammable
+            return Math.max(resultFlammability, 3);
+        }
+        return super.getFlammability(state, level, pos, direction);
+    }
+
+    @Override
+    public int getFireSpreadSpeed(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        if (level.getBlockEntity(pos) instanceof NeoplasmRotBlockEntity be) {
+            // Better copy fire spread speed from original
+            BlockState original = be.getOriginalState();
+            int originalFireSpreadSpeed = original.getFlammability(level, pos, direction);
+
+            int resultFireSpreadSpeed = (int) Math.ceil(originalFireSpreadSpeed * be.getMultiplier(FLAME_MULTIPLIERS));
+
+            // Always allow spread fire
+            return Math.max(resultFireSpreadSpeed, 1);
+        }
+        return super.getFireSpreadSpeed(state, level, pos, direction);
+    }
+
+    @Override
+    public float getExplosionResistance(BlockState state, BlockGetter level, BlockPos pos, Explosion explosion) {
+        if (level.getBlockEntity(pos) instanceof NeoplasmRotBlockEntity be) {
+            // Better copy explosion resistance from original
+            BlockState original = be.getOriginalState();
+            float originalResistance = original.getExplosionResistance(level, pos, explosion);
+
+            return originalResistance * be.getMultiplier(EXPLOSION_RESISTANCE_MULTIPLIERS);
+        }
+        return super.getExplosionResistance(state, level, pos, explosion);
+    }
+
+    @Override
+    public SoundType getSoundType(BlockState state, LevelReader level, BlockPos pos, @Nullable Entity entity) {
+        if (level.getBlockEntity(pos) instanceof NeoplasmRotBlockEntity be) {
+            // Also on first stage better copy original sounds
+            // because infection is not that strong
+            if (be.getOverlayStage() < 1) {
+                return be.getOriginalState().getSoundType();
+            }
+        }
+        return super.getSoundType(state, level, pos, entity); // (slime sound) Maybe make custom sound?
+    }
+
+    @Override
+    public void fallOn(Level level, @NotNull BlockState state, @NotNull BlockPos pos, @NotNull Entity entity, float fallDistance) {
+        if (level.getBlockEntity(pos) instanceof NeoplasmRotBlockEntity be) {
+            // Due to the fleshy origin of the neoplasm
+            // we reduce damage depending on the stage of infection
+            super.fallOn(level, state, pos, entity, fallDistance * be.getMultiplier(FALL_DAMAGE_MULTIPLIERS));
+        } else {
+            super.fallOn(level, state, pos, entity, fallDistance);
+        }
+    }
 }
+
