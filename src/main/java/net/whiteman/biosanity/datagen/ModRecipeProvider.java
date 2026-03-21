c68a8f8d0a232b225d803565f287b55cce285173
WhiteManXYZ <vladimir.gavrilyuk10@gmail.com>
2026-03-21 19:15:05 +0100

    - Purifier recipe datagen

--- src/main/java/net/whiteman/biosanity/datagen/ModRecipeProvider.java
+++ src/main/java/net/whiteman/biosanity/datagen/ModRecipeProvider.java
@@ -2,29 +2,62 @@ package net.whiteman.biosanity.datagen;
 
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.AbstractCookingRecipe;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 import net.whiteman.biosanity.BiosanityMod;
 import net.whiteman.biosanity.block.ModBlocks;
+import net.whiteman.biosanity.datagen.recipe_builders.PurificationStationRecipeBuilder;
 import net.whiteman.biosanity.item.ModItems;
+import net.whiteman.biosanity.recipe.ModRecipes;
+import net.whiteman.biosanity.util.block.purification_station.ColorsRegistry;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.Consumer;
+
+import static net.whiteman.biosanity.util.block.purification_station.ModifiersUtils.ModifierType;
 
 public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
     private static final List<ItemLike> ALGANIT_SMELTABLES = List.of(ModBlocks.NETHER_ALGANIT_ORE.get());
+    private static final String PURIFICATION_PATH = "purifier_recipes/purification/";
+    private static final String PAINTING_PATH = "purifier_recipes/painting/";
+
+    private static final Map<String, TagKey<Item>> PAINTING_FOLDERS = Map.ofEntries(
+            Map.entry("wool", ItemTags.WOOL),
+            Map.entry("wool_carpets", ItemTags.WOOL_CARPETS),
+            Map.entry("glass", Tags.Items.GLASS),
+            Map.entry("concrete_powder", Tags.Items.SAND),
+            Map.entry("glass_panes", Tags.Items.GLASS_PANES),
+            Map.entry("candle", ItemTags.CANDLES),
+            Map.entry("shulkers", ItemTags.create(new ResourceLocation("minecraft", "shulker_boxes"))),
+            Map.entry("terracotta", ItemTags.TERRACOTTA),
+            Map.entry("beds", ItemTags.BEDS),
+            Map.entry("banners", ItemTags.BANNERS),
+            Map.entry("concrete", ModTags.Items.CONCRETE)
+    );
+
+    private static final Set<Item> EXCLUDED_ITEMS = Set.of(Items.TINTED_GLASS);
 
     public ModRecipeProvider(PackOutput pOutput) {
         super(pOutput);
     }
 
     @Override
-    protected void buildRecipes(Consumer<FinishedRecipe> pWriter) {
+    protected void buildRecipes(@NotNull Consumer<FinishedRecipe> pWriter) {
+        //region ------------ Vanilla recipes datagen
         ShapedRecipeBuilder.shaped(RecipeCategory.REDSTONE, ModBlocks.UV_LAMP_BLOCK.get())
                 .pattern("IAI")
                 .pattern("A#A")
@@ -49,23 +82,64 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
 
         oreSmelting(pWriter, ALGANIT_SMELTABLES, RecipeCategory.MISC, ModItems.ALGANIT.get(), 0, 200, "alganit");
         oreBlasting(pWriter, ALGANIT_SMELTABLES, RecipeCategory.MISC, ModItems.ALGANIT.get(), 0, 100, "alganit");
+        //endregion
+        
+        //region ------------ Custom recipes datagen
+        PurificationStationRecipeBuilder.recipe(
+                ModItems.PURIFIED_ALGANIT.get(),
+                Ingredient.of(ModItems.ALGANIT.get()),
+                ModifierType.SAND_DUST,
+                1,
+                400,
+                DyeColor.WHITE,
+                ModRecipes.PURIFICATION_SERIALIZER.get()
+        ).save(pWriter, new ResourceLocation(BiosanityMod.MOD_ID,
+                PURIFICATION_PATH + "purification_" + ModItems.ALGANIT.get()));
+
+        for (DyeColor color : DyeColor.values()) {
+            PAINTING_FOLDERS.forEach((folderName, tag) -> {
+                List<Item> folderItems = ColorsRegistry.getListByFolder(folderName);
+                Item result = ColorsRegistry.getBlockForColor(folderItems, color);
+
+                if (result != Items.AIR) {
+                    List<Item> filteredItems = folderItems.stream()
+                            .filter(item -> !EXCLUDED_ITEMS.contains(item))
+                            .filter(item -> item != result)
+                            .toList();
+
+                    if (!filteredItems.isEmpty()) {
+                        Ingredient listIngredient = Ingredient.of(filteredItems.toArray(new Item[0]));
+                        PurificationStationRecipeBuilder.recipe(
+                                result,
+                                listIngredient,
+                                ModifierType.DYE,
+                                1,
+                                100,
+                                color,
+                                ModRecipes.PAINTING_SERIALIZER.get()
+                        ).save(pWriter, new ResourceLocation(BiosanityMod.MOD_ID,
+                                PAINTING_PATH + folderName + "/" + color.getName()));
+                    }
+                }
+            });
+        }
+        //endregion
     }
 
-    protected static void oreSmelting(Consumer<FinishedRecipe> pFinishedRecipeConsumer, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTIme, String pGroup) {
+    protected static void oreSmelting(@NotNull Consumer<FinishedRecipe> pFinishedRecipeConsumer, List<ItemLike> pIngredients, @NotNull RecipeCategory pCategory, @NotNull ItemLike pResult, float pExperience, int pCookingTIme, @NotNull String pGroup) {
         oreCooking(pFinishedRecipeConsumer, RecipeSerializer.SMELTING_RECIPE, pIngredients, pCategory, pResult, pExperience, pCookingTIme, pGroup, "_from_smelting");
     }
 
-    protected static void oreBlasting(Consumer<FinishedRecipe> pFinishedRecipeConsumer, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTime, String pGroup) {
+    protected static void oreBlasting(@NotNull Consumer<FinishedRecipe> pFinishedRecipeConsumer, List<ItemLike> pIngredients, @NotNull RecipeCategory pCategory, @NotNull ItemLike pResult, float pExperience, int pCookingTime, @NotNull String pGroup) {
         oreCooking(pFinishedRecipeConsumer, RecipeSerializer.BLASTING_RECIPE, pIngredients, pCategory, pResult, pExperience, pCookingTime, pGroup, "_from_blasting");
     }
 
-    protected static void oreCooking(Consumer<FinishedRecipe> pFinishedRecipeConsumer, RecipeSerializer<? extends AbstractCookingRecipe> pCookingSerializer, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTime, String pGroup, String pRecipeName) {
+    protected static void oreCooking(@NotNull Consumer<FinishedRecipe> pFinishedRecipeConsumer, @NotNull RecipeSerializer<? extends AbstractCookingRecipe> pCookingSerializer, List<ItemLike> pIngredients, @NotNull RecipeCategory pCategory, @NotNull ItemLike pResult, float pExperience, int pCookingTime, @NotNull String pGroup, String pRecipeName) {
         for(ItemLike itemlike : pIngredients) {
             SimpleCookingRecipeBuilder.generic(Ingredient.of(itemlike), pCategory, pResult,
                     pExperience, pCookingTime, pCookingSerializer)
                     .group(pGroup).unlockedBy(getHasName(itemlike), has(itemlike))
                     .save(pFinishedRecipeConsumer,  BiosanityMod.MOD_ID + ":" + getItemName(pResult) + pRecipeName + "_" + getItemName(itemlike));
         }
-
     }
 }
---
+++ src/main/java/net/whiteman/biosanity/datagen/recipe_builders/PurificationStationRecipeBuilder.java
@@ -0,0 +1,109 @@
+package net.whiteman.biosanity.datagen.recipe_builders;
+
+import com.google.gson.JsonObject;
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
+import java.util.function.Consumer;
+
+import static net.whiteman.biosanity.util.block.purification_station.ModifiersUtils.ModifierType;
+
+public class PurificationStationRecipeBuilder {
+    private final Item result;
+    private final Ingredient ingredient;
+    private final ModifierType modifier;
+    private final DyeColor color;
+    private final int count;
+    private final int processingTime;
+    private final RecipeSerializer<?> serializer;
+
+    public PurificationStationRecipeBuilder(Item result, Ingredient ingredient, ModifierType modifier, int count, int processingTime, DyeColor color, RecipeSerializer<?> serializer) {
+        this.result = result;
+        this.ingredient = ingredient;
+        this.modifier = modifier;
+        this.color = color;
+        this.count = count;
+        this.processingTime = processingTime;
+        this.serializer = serializer;
+    }
+
+    /**
+     * Purification station recipe generator.
+     * @param result Output item.
+     * @param ingredient Input item.
+     * @param modifier Modifier type (sand, dye e.t.c)
+     * @param count Output item count.
+     * @param time Purification or painting time e.g.
+     * @param color Modifier color. If recipe need no color, set always to DyeColor.WHITE
+     * @param serializer Type of recipe.
+     */
+    public static PurificationStationRecipeBuilder recipe(Item result, Ingredient ingredient, ModifierType modifier, int count, int time, DyeColor color, RecipeSerializer<?> serializer) {
+        return new PurificationStationRecipeBuilder(result, ingredient, modifier, count, time, color, serializer);
+    }
+
+    public void save(Consumer<FinishedRecipe> consumer, ResourceLocation id) {
+        consumer.accept(new Result(id, this.result, this.ingredient, this.modifier, this.count, this.processingTime, this.color, this.serializer));
+    }
+
+    public static class Result implements FinishedRecipe {
+        private final ResourceLocation id;
+        private final Item result;
+        private final Ingredient ingredient;
+        private final ModifierType modifier;
+        private final DyeColor color;
+        private final int count;
+        private final int processingTime;
+        private final RecipeSerializer<?> serializer;
+
+        public Result(ResourceLocation id, Item result, Ingredient ingredient, ModifierType modifier, int count, int processingTime, DyeColor color, RecipeSerializer<?> serializer) {
+            this.id = id;
+            this.result = result;
+            this.ingredient = ingredient;
+            this.modifier = modifier;
+            this.color = color;
+            this.count = count;
+            this.processingTime = processingTime;
+            this.serializer = serializer;
+        }
+
+        @Override
+        public void serializeRecipeData(JsonObject json) {
+            json.add("ingredient", this.ingredient.toJson());
+            json.addProperty("modifier", this.modifier.name().toLowerCase());
+            json.addProperty("color", this.color.name().toLowerCase());
+
+            JsonObject resultJson = new JsonObject();
+            resultJson.addProperty("item", Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(this.result), "Item not registered: " + this.result).toString());
+
+            resultJson.addProperty("count", this.count);
+            json.add("result", resultJson);
+            json.addProperty("processingTime", this.processingTime);
+        }
+
+        @Override
+        public @NotNull ResourceLocation getId() {
+            return this.id;
+        }
+
+        @Override
+        public @NotNull RecipeSerializer<?> getType() {
+            return this.serializer;
+        }
+
+        @Nullable
+        @Override
+        public JsonObject serializeAdvancement() { return null; }
+
+        @Nullable
+        @Override
+        public ResourceLocation getAdvancementId() { return null; }
+    }
+}
