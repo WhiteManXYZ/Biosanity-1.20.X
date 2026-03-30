e1686acfb4d7653ed6337ec6eb9f5e052de1a90b
WhiteManXYZ <vladimir.gavrilyuk10@gmail.com>
2026-03-30 18:16:03 +0200

    - First primitive core logic added

--- src/main/java/net/whiteman/biosanity/block/entity/custom/NeoplasmCoreBlockEntity.java
+++ src/main/java/net/whiteman/biosanity/block/entity/custom/NeoplasmCoreBlockEntity.java
@@ -10,24 +10,75 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.whiteman.biosanity.block.entity.ModBlockEntities;
 import org.jetbrains.annotations.NotNull;
 
-import static net.whiteman.biosanity.util.block.NeoplasmRegistry.CoreLevel;
+import static net.whiteman.biosanity.util.block.NeoplasmRegistry.*;
 
 public class NeoplasmCoreBlockEntity extends BlockEntity {
+    public static final int CALM_DOWN_RATE = 5;
+    public static final int RESOURCE_LEVEL_MULTIPLIER = 10;
+
+    private static final int[] XP_TABLE = {3, 10, 30, 90, 270, 810, 2430};
+
     public NeoplasmCoreBlockEntity(BlockPos pPos, BlockState pBlockState) {
         super(ModBlockEntities.NEOPLASM_CORE_BE.get(), pPos, pBlockState);
     }
 
+    // TODO make different variants for different systems
     private int currentXp = CoreLevel.getStartingXp();
     private int alertPoints = 0;
-    private int alertLevel; // TODO enum type
+    private CoreAlertLevel alertLevel;
 
     public void tick(Level level, BlockPos pos, BlockState state, NeoplasmCoreBlockEntity blockEntity) {
-        if (level.hasNearbyAlivePlayer(pos.getX(), pos.getY(), pos.getZ(), 10D)) alertPoints++;
-        else alertPoints--;
-        System.out.println("[NP: CORE] ticking. " + currentXp + " current experience, " + "alert points: " + alertPoints);
+
+
+        // Every 1-second block
+        if (level.getGameTime() % 20 == 0) {
+            // Calm down over time
+            blockEntity.decreaseAlertPoints(CALM_DOWN_RATE);
+        }
     }
 
-    protected void spreadInfection(BlockState pState, ServerLevel pLevel, BlockPos pPos, RandomSource pRandom) {}
+    private void spreadInfection(BlockState pState, ServerLevel pLevel, BlockPos pPos, RandomSource pRandom) {}
+
+    public void increaseAlertPoints(int amount) {
+        if (this.alertPoints >= MAX_ALERT_POINTS) return;
+
+        this.alertPoints = Math.min(alertPoints + amount, MAX_ALERT_POINTS);
+        this.alertLevel = CoreAlertLevel.fromPoints(this.alertPoints);
+    }
+
+    public void decreaseAlertPoints(int amount) {
+        if (this.alertPoints <= 0) return;
+
+        this.alertPoints = Math.max(alertPoints - amount, 0);
+        this.alertLevel = CoreAlertLevel.fromPoints(this.alertPoints);
+    }
+
+    public void decomposeResource(int level, ResourceType type) {
+        switch (type) {
+            case BIOMASS -> {
+                // Some code
+                System.out.println("gained biomass");
+                System.out.println(level);
+
+            }
+            case MINERAL -> {
+                // Some code
+                System.out.println("gained mineral");
+                System.out.println(level);
+
+            }
+            case ENERGY -> {
+                // Some code
+                System.out.println("gained energy");
+                System.out.println(level);
+            }
+        }
+        System.out.println("With level " + level + " result xp: " + calculateXp(level));
+    }
+
+    private int calculateXp(int level) {
+        return (int) (RESOURCE_LEVEL_MULTIPLIER * Math.pow(3, level - 1));
+    }
 
     @Override
     protected void saveAdditional(@NotNull CompoundTag pTag) {
