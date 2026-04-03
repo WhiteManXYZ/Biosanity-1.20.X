ed4ae842d1320869d34914ce26986a75072e7b55
WhiteManXYZ <vladimir.gavrilyuk10@gmail.com>
2026-04-03 03:17:54 +0200

    - Purifier spectator cant open menu fixed - Quick move fix + vanilla parity

--- src/main/java/net/whiteman/biosanity/world/inventory/PurificationStationMenu.java
+++ src/main/java/net/whiteman/biosanity/world/inventory/PurificationStationMenu.java
@@ -9,114 +9,149 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.items.ItemStackHandler;
 import net.minecraftforge.items.SlotItemHandler;
+import net.whiteman.biosanity.world.item.ModItems;
 import net.whiteman.biosanity.world.level.block.ModBlocks;
 import net.whiteman.biosanity.world.level.block.entity.PurificationStationBlockEntity;
-import net.whiteman.biosanity.world.item.ModItems;
 import net.whiteman.biosanity.world.util.ColoredItemsRegistry;
 import org.jetbrains.annotations.NotNull;
 
 import static net.whiteman.biosanity.world.util.ModifierUtils.ModifierType;
 
 public class PurificationStationMenu extends AbstractContainerMenu {
+    public static final int INGREDIENT_SLOT = 0;
+    public static final int FUEL_SLOT = 1;
+    public static final int MODIFIER_SLOT = 2;
+    public static final int RESULT_SLOT = 3;
+    public static final int SLOT_COUNT = 4;
     public final PurificationStationBlockEntity blockEntity;
     private final Level level;
     private final ContainerData data;
 
     public PurificationStationMenu(int pContainerId, Inventory inv, FriendlyByteBuf extraData) {
-        // TODO(whiteman) fix when spectator try to open menu, it wont open + error in log
-        this(pContainerId, inv, inv.player.level().getBlockEntity(extraData.readBlockPos()), new SimpleContainerData(9));
+        this(pContainerId, inv, (extraData != null) ? inv.player.level().getBlockEntity(extraData.readBlockPos()) : null, new SimpleContainerData(9));
     }
 
     public PurificationStationMenu(int pContainerId, Inventory inv, BlockEntity entity, ContainerData data) {
         super(ModMenuTypes.PURIFICATION_STATION_BLOCK_MENU.get(), pContainerId);
-        checkContainerSize(inv, 4);
-        blockEntity = ((PurificationStationBlockEntity) entity);
+        checkContainerSize(inv, SLOT_COUNT);
+        if (entity instanceof PurificationStationBlockEntity be) {
+            this.blockEntity = be;
+        } else {
+            this.blockEntity = null;
+        }
         this.level = inv.player.level();
         this.data = data;
 
         addPlayerInventory(inv);
         addPlayerHotbar(inv);
 
-        this.blockEntity.getCapability(ForgeCapabilities.ITEM_HANDLER).ifPresent(iItemHandler -> {
-            this.addSlot(new SlotItemHandler(iItemHandler, 0, 71, 40));
-            this.addSlot(new SlotItemHandler(iItemHandler, 1, 127, 40));
-            this.addSlot(new SlotItemHandler(iItemHandler, 2, 17, 43));
-            this.addSlot(new SlotItemHandler(iItemHandler, 3, 17, 17));
-        });
+        if (this.blockEntity != null) {
+            this.blockEntity.getCapability(ForgeCapabilities.ITEM_HANDLER).ifPresent(iItemHandler -> {
+                this.addSlot(new SlotItemHandler(iItemHandler, 0, 71, 40));
+                this.addSlot(new SlotItemHandler(iItemHandler, 1, 17, 43));
+                this.addSlot(new SlotItemHandler(iItemHandler, 2, 17, 17));
+                this.addSlot(new SlotItemHandler(iItemHandler, 3, 127, 40));
+            });
+        } else {
+            // CLIENT STUB: If BE is null, create empty slots
+            ItemStackHandler dummy = new ItemStackHandler(4);
+            this.addSlot(new SlotItemHandler(dummy, 0, 71, 40));
+            this.addSlot(new SlotItemHandler(dummy, 1, 17, 43));
+            this.addSlot(new SlotItemHandler(dummy, 2, 17, 17));
+            this.addSlot(new SlotItemHandler(dummy, 3, 127, 40));
+        }
 
         addDataSlots(data);
     }
 
-    // CREDIT GOES TO: diesieben07 | https://github.com/diesieben07/SevenCommons
-    // must assign a slot number to each of the slots used by the GUI.
+    // I rechecked this and in my case I want use vanilla system, for parity (im obsessed with this lol).
+    // So I took the original code and rewrote it to suit my needs.
     // For this container, we can see both the tile inventory's slots as well as the player inventory slots and the hotbar.
     // Each time we add a Slot to the container, it automatically increases the slotIndex, which means
-    //  0 - 8 = hotbar slots (which will map to the InventoryPlayer slot numbers 0 - 8)
-    //  9 - 35 = player inventory slots (which map to the InventoryPlayer slot numbers 9 - 35)
-    //  36 - 44 = TileInventory slots, which map to our TileEntity slot numbers 0 - 8)
-    private static final int HOTBAR_SLOT_COUNT = 9;
-    private static final int PLAYER_INVENTORY_ROW_COUNT = 3;
-    private static final int PLAYER_INVENTORY_COLUMN_COUNT = 9;
-    private static final int PLAYER_INVENTORY_SLOT_COUNT = PLAYER_INVENTORY_COLUMN_COUNT * PLAYER_INVENTORY_ROW_COUNT;
-    private static final int VANILLA_SLOT_COUNT = HOTBAR_SLOT_COUNT + PLAYER_INVENTORY_SLOT_COUNT;
-    private static final int VANILLA_FIRST_SLOT_INDEX = 0;
-    private static final int TE_INVENTORY_FIRST_SLOT_INDEX = VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT;
-
-    private static final int TE_INVENTORY_SLOT_COUNT = 4;  // must be the number of slots you have!
+    //  0 - 26 = player inventory slots (which map to the InventoryPlayer slot numbers 0 - 26)
+    //  27 - 35 = hotbar slots (which will map to the InventoryPlayer slot numbers 27 - 35)
+    //  36 - 39 = TileInventory slots, which map to our TileEntity slot numbers 36 - 39)
+    public static final int INVENTORY_SLOT_COUNT = 36;
+    public static final int INGREDIENT_SLOT_IN_ORDER = INVENTORY_SLOT_COUNT + INGREDIENT_SLOT;
+    public static final int FUEL_SLOT_IN_ORDER = INVENTORY_SLOT_COUNT + FUEL_SLOT;
+    public static final int MODIFIER_SLOT_IN_ORDER = INVENTORY_SLOT_COUNT + MODIFIER_SLOT;
+    public static final int RESULT_SLOT_IN_ORDER = INVENTORY_SLOT_COUNT + RESULT_SLOT;
     @Override
-    public @NotNull ItemStack quickMoveStack(@NotNull Player playerIn, int pIndex) {
-        Slot sourceSlot = slots.get(pIndex);
-        if (sourceSlot == null || !sourceSlot.hasItem()) return ItemStack.EMPTY;  //EMPTY_ITEM
-        ItemStack sourceStack = sourceSlot.getItem();
-        ItemStack copyOfSourceStack = sourceStack.copy();
+    public @NotNull ItemStack quickMoveStack(@NotNull Player pPlayer, int clickedSlotIndex) {
+        ItemStack itemstack = ItemStack.EMPTY;
+        Slot slot = this.slots.get(clickedSlotIndex);
+        if (slot.hasItem()) {
+            ItemStack itemstack1 = slot.getItem();
+            itemstack = itemstack1.copy();
+            if (clickedSlotIndex == RESULT_SLOT_IN_ORDER) {
+                if (!this.moveItemStackTo(itemstack1, 0, 36, false)) {
+                    return ItemStack.EMPTY;
+                }
 
-        // Check if the slot clicked is one of the vanilla container slots
-        if (pIndex < VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT) {
-            // This is a vanilla container slot so merge the stack into the tile inventory
-            // TODO(whiteman) make vanilla parity?
-            if (PurificationStationBlockEntity.ALLOWED_FUEL.test(sourceStack)) {
-                if (!moveItemStackTo(sourceStack, TE_INVENTORY_FIRST_SLOT_INDEX + 2, TE_INVENTORY_FIRST_SLOT_INDEX + 3, false)) {
+                slot.onQuickCraft(itemstack1, itemstack);
+            } else if (clickedSlotIndex != INGREDIENT_SLOT_IN_ORDER && clickedSlotIndex != FUEL_SLOT_IN_ORDER & clickedSlotIndex != MODIFIER_SLOT_IN_ORDER) {
+                if (this.canProcess(itemstack1)) {
+                    if (!this.moveItemStackTo(itemstack1, 36, 37, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else if (this.isFuel(itemstack1)) {
+                    if (!this.moveItemStackTo(itemstack1, 37, 38, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else if (this.isModifier(itemstack1)) {
+                    if (!this.moveItemStackTo(itemstack1, 38, 39, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                    // Clicked in inventory case
+                } else if (clickedSlotIndex < 27) {
+                    if (!this.moveItemStackTo(itemstack1, 27, 36, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                    // Clicked in hotbar case
+                } else if (clickedSlotIndex < 36 && !this.moveItemStackTo(itemstack1, 0, 27, false)) {
                     return ItemStack.EMPTY;
                 }
-            }
-            else if (PurificationStationBlockEntity.ALLOWED_MODIFICATORS.test(sourceStack)) {
-                if (!moveItemStackTo(sourceStack, TE_INVENTORY_FIRST_SLOT_INDEX + 3, TE_INVENTORY_FIRST_SLOT_INDEX + 4, false)) {
-                    return ItemStack.EMPTY;
-                }
-            }
-
-            else if (sourceStack.is(ModItems.ALGANIT.get()) || ColoredItemsRegistry.isPaintable(sourceStack)) {
-                if (!moveItemStackTo(sourceStack, TE_INVENTORY_FIRST_SLOT_INDEX, TE_INVENTORY_FIRST_SLOT_INDEX + 1, false)) {
-                    return ItemStack.EMPTY;
-                }
-            }
-            else {
+                // Move to inventory/hotbar (bc we clicked in our TileEntity menu)
+            } else if (!this.moveItemStackTo(itemstack1, 0, 35, false)) {
                 return ItemStack.EMPTY;
             }
-        } else if (pIndex < TE_INVENTORY_FIRST_SLOT_INDEX + TE_INVENTORY_SLOT_COUNT) {
-            // This is a TE slot so merge the stack into the players inventory
-            if (!moveItemStackTo(sourceStack, VANILLA_FIRST_SLOT_INDEX, VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT, false)) {
+
+            if (itemstack1.isEmpty()) {
+                slot.setByPlayer(ItemStack.EMPTY);
+            } else {
+                slot.setChanged();
+            }
+
+            if (itemstack1.getCount() == itemstack.getCount()) {
                 return ItemStack.EMPTY;
             }
-        } else {
-            System.out.println("Invalid slotIndex:" + pIndex);
-            return ItemStack.EMPTY;
+
+            slot.onTake(pPlayer, itemstack1);
         }
-        // If stack size == 0 (the entire stack was moved) set slot contents to null
-        if (sourceStack.getCount() == 0) {
-            sourceSlot.set(ItemStack.EMPTY);
-        } else {
-            sourceSlot.setChanged();
-        }
-        sourceSlot.onTake(playerIn, sourceStack);
-        return copyOfSourceStack;
+
+        return itemstack;
     }
 
     @Override
     public boolean stillValid(@NotNull Player pPlayer) {
+        if (pPlayer.isSpectator()) return true;
+
         return stillValid(ContainerLevelAccess.create(level, blockEntity.getBlockPos()),
                 pPlayer, ModBlocks.PURIFICATION_STATION_BLOCK.get());
+    }
+
+    private boolean canProcess(ItemStack pStack) {
+        return pStack.is(ModItems.ALGANIT.get()) || ColoredItemsRegistry.isPaintable(pStack);
+    }
+
+    private boolean isFuel(ItemStack pStack) {
+        return PurificationStationBlockEntity.ALLOWED_FUEL.test(pStack);
+    }
+
+    private boolean isModifier(ItemStack pStack) {
+        return PurificationStationBlockEntity.ALLOWED_MODIFICATORS.test(pStack);
     }
 
     private void addPlayerInventory(Inventory playerInventory) {
